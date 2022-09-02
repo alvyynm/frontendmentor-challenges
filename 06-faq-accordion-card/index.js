fb2ea0b4bd4e1bb85d0990176a3470d9dc6ce03d
alvyynm <alvyynm@gmail.com>
2022-09-02 05:48:54 +0300

    ✨ Hide/reveal accordion on click

--- 06-faq-accordion-card/index.js
+++ 06-faq-accordion-card/index.js
@@ -7,8 +7,14 @@ const answers = document.querySelectorAll(".answers");
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
-    answers[i].classList.remove("hidden");
-    btnQuestion[i].classList.add("active");
-    accordionIcon[i].classList.add("rotate-icon");
+    if (answers[i].classList.contains("hidden")) {
+      answers[i].classList.remove("hidden");
+      btnQuestion[i].classList.add("active");
+      accordionIcon[i].classList.add("rotate-icon");
+    } else {
+      answers[i].classList.add("hidden");
+      btnQuestion[i].classList.remove("active");
+      accordionIcon[i].classList.remove("rotate-icon");
+    }
   });
 }
