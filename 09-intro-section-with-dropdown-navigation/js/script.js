8dfb9398fb9c7fe304b325f42c9457e9420e65d9
alvyynm <alvyynm@gmail.com>
2022-09-28 12:46:12 +0300

    🛠 Switch dropdown icon on hover

--- 09-intro-section-with-dropdown-navigation/js/script.js
+++ 09-intro-section-with-dropdown-navigation/js/script.js
@@ -5,6 +5,7 @@ const primaryNav = document.querySelector(".primary-navigation");
 const hamburgerBtn = document.querySelector(".icon-hamburger");
 const closeBtn = document.querySelector(".icon-close");
 const menuNavToggle = document.querySelectorAll(".menu-dropdown-toggle");
+const icons = document.querySelectorAll(".updownarrow");
 
 // Event handlers
 
@@ -22,3 +23,22 @@ navToggle.addEventListener("click", function () {
 //     toggle.classList.toggle("reveal");
 //   });
 // });
+
+// Switch displayed arrows in nav buttons
+menuNavToggle.forEach((el) => {
+  el.addEventListener("mouseenter", () => {
+    icons.forEach((icon, i) => {
+      if (el.dataset.num === icon.dataset.id) {
+        icon.setAttribute("src", "./images/icon-arrow-up.svg");
+      }
+    });
+  });
+});
+
+menuNavToggle.forEach((el) => {
+  el.addEventListener("mouseleave", () => {
+    icons.forEach((icon, i) => {
+      icon.setAttribute("src", "./images/icon-arrow-down.svg");
+    });
+  });
+});
