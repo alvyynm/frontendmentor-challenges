56d6da7b79233a53399c6732f0f2e692ae91b5a1
alvyynm <alvyynm@gmail.com>
2023-01-05 23:21:15 +0300

    feat: Save theme in localStorage and use it on revisits or page reloads

--- 12-rest-countries-api/src/contexts/DarkModeContext.jsx
+++ 12-rest-countries-api/src/contexts/DarkModeContext.jsx
@@ -1,15 +1,35 @@
-import React, { useState, createContext } from "react";
+import React, { useState, useEffect, createContext } from "react";
 
 export const ThemeContext = createContext();
 
 export const ThemeContextProvider = ({ children }) => {
   // Global states
-  const [isLightTheme, setIsLightTheme] = useState(true);
+  const [isLightTheme, setIsLightTheme] = useState("");
 
   // Theme toggler
   const toggleTheme = () => {
     setIsLightTheme(!isLightTheme);
   };
+
+  // Save user theme option in localStorage
+  useEffect(() => {
+    localStorage.setItem("lightTheme", JSON.stringify(isLightTheme));
+  }, [isLightTheme]);
+
+  // Check if user has a custom theme selected and use it as the default
+  // If user sets lightTheme to true or false, it's rembered in the next visit or page reload
+
+  useEffect(() => {
+    const lightTheme = JSON.parse(localStorage.getItem("lightTheme"));
+    if (lightTheme === "") {
+      // do nothing
+    } else if (lightTheme === false) {
+      setIsLightTheme(false);
+    } else {
+      setIsLightTheme(true);
+    }
+  }, []);
+
   return (
     <ThemeContext.Provider
       value={{
