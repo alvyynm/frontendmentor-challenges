6e3fde192229f2acc013ac5d852ace92c5cf5fac
alvyynm <alvyynm@gmail.com>
2023-01-06 23:14:28 +0300

    feat: allow user to click through to the border countries on the detail page

--- 12-rest-countries-api/src/components/CountryInfo.jsx
+++ 12-rest-countries-api/src/components/CountryInfo.jsx
@@ -11,6 +11,7 @@ export default function CountryInfo({ countryName }) {
   const [countryDetails, setCountryDetails] = useState([]);
   const { isLightTheme } = useContext(ThemeContext);
   const [countryData, setCountryData] = useState("");
+  const [countryCode, setCountryCode] = useState("");
 
   // Request country specific info when user clicks on a country
 
@@ -21,17 +22,20 @@ export default function CountryInfo({ countryName }) {
         setCountryDetails(response.data[0]);
       })
       .catch((error) => console.log(error));
-  }, [countryName]);
+  }, []);
 
-  // useEffect(() => {
-  //   axios
-  //     .get(`https://restcountries.com/v3.1/alpha/${item}`)
-  //     .then((response) => {
-  //       setCountryData(response.data[0]);
-  //       console.log(countryData);
-  //     })
-  //     .catch((error) => console.log(error));
-  // }, []);
+  // updates countryDetails when user clicks on a border country
+
+  useEffect(() => {
+    if (countryCode) {
+      axios
+        .get(`https://restcountries.com/v3.1/alpha/${countryCode}`)
+        .then((response) => {
+          setCountryDetails(response.data[0]);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [countryCode]);
 
   if (countryDetails.length === 0) {
     return <div className="font-primary">Loading data...</div>;
@@ -129,15 +133,18 @@ export default function CountryInfo({ countryName }) {
                 <span className="font-semibold mr-4">Border Countries:</span>
                 {countryDetails.borders === undefined
                   ? "Island (no border countries)"
-                  : countryDetails.borders?.map((item) => {
+                  : countryDetails.borders?.map((item, i) => {
                       return (
                         <button
                           key={uuidv4()}
                           className={`text-white rounded-md shadow-md px-5 py-1 mr-2 mb-2 ${
                             isLightTheme ? "lightels" : "darkels"
                           }`}
+                          onClick={() => {
+                            setCountryCode(item);
+                          }}
                         >
-                          {item}
+                          <Link to={`/country/${i}`}>{item}</Link>
                         </button>
                       );
                     })}
